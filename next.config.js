85e81fc3a607adfeb4977da3dfeaf8d56b7785db
heiwa4126 <heiwa4126@gmail.com>
2023-07-21 15:58:57 +0900

    added authentication functionality using NextAuth.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
+
+// workaround for 'UNABLE_TO_GET_ISSUER_CERT_LOCALLY'
+// https://github.com/nextauthjs/next-auth/issues/3580
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
 const nextConfig = {
   reactStrictMode: true
 };
